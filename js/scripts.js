de7524fefec0d1f31b6b14c3c3552dce42072da4
Eva Wiedmann <evawiedmann@gmail.com>
2019-11-08 10:45:27 -0800

    map out backend logic, experiment, get functional

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
 // Business Logic
 
+function Pizza(toppings,size) {
+this.toppings = toppings;
+this.size = size;
+this.price = 0;
+}
+
+Pizza.prototype.calcPrice = function () {
+  this.toppings.forEach(function(toppings) {
+    this.price += 1;
+  }
+  if (this.size === 12) {
+this.price += 5
+} else if (this.size === 6) {
+  this.price += 3;
+}
+return this.price;
+)
+};
 
 
 // User Interface
@@ -7,7 +25,7 @@
 $(document).ready(function() {
   $("form#").submit(function(event) {
     event.preventDefault();
-    
+
     });
   });
 });
