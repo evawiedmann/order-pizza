b2c86d322ffe3852e8f654ac23dfabec3ffd8a6c
Eva Wiedmann <evawiedmann@gmail.com>
2019-11-08 15:39:36 -0800

    was able to get topping selections into an array without looping, might try to separate them all out, not too DRY but might work

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Business Logic
 function Pizza(toppings,size) {
-  this.toppings = [];
+  this.toppings = toppings;
   this.size = size;
   this.price = 0;
 }
@@ -10,15 +10,15 @@ Pizza.prototype.calcPrice = function () {
     this.price += 1;
   })
   if (this.size === 24) {
-    this.price += 9;
+    this.price += 30;
   } else if (this.size === 18) {
-    this.price +=7;
+    this.price +=25;
   } else if (this.size === 14) {
-    this.price += 5;
+    this.price += 23;
   } else if (this.size === 11) {
-    this.price += 3;
+    this.price += 20;
   } else if (this.size === 8) {
-    this.price += 3;
+    this.price += 15;
   }
   return this.price;
 };
@@ -30,26 +30,19 @@ $(document).ready(function() {
   $("form#order").submit(function(event) {
     event.preventDefault();
 
+    let toppingsInputs = ($("#toppings:checked").val());
     let toppingsArray = [];
+    let sizeInput = parseInt($("select#size").val());
 
-  let toppingsInputs = $("input:checkbox[name=type]:checked").each(function(){
-    toppingsArray.push($(this).val());
-});
-
-    // let toppingsInputs = $("input:checked").val();
-    console.log(typeof toppingsInputs);
-    let sizeInput = $("select#size").val();
-
-// toppingsArray.forEach(function(toppingsInput) {
-//   toppingsArray.push(toppingsInputs);
-// });
-
-      let ourPizza = new Pizza(toppingsArray, sizeInput);
-      console.log(ourPizza);
-      let price = ourPizza.calcPrice();
-
-      $("#order").hide();
-      $('#priceOutput').show();
-      $("#output").text(price);
+    toppingsArray.forEach(function(toppingsInput) {
+      toppingsArray.push(toppingsInputs);
     });
+
+    let ourPizza = new Pizza(toppingsArray, sizeInput);
+    let price = ourPizza.calcPrice();
+
+    $("#order").hide();
+    $('#priceOutput').show();
+    $("#output").text(price);
   });
+});
