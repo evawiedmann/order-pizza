c5ee2b8d2d012f560d004db8334c9fc0491b3898
Eva Wiedmann <evawiedmann@gmail.com>
2019-11-08 12:47:01 -0800

    change forEach loop to for loop in UI, not fully working

--- js/scripts.js
+++ js/scripts.js
@@ -9,9 +9,13 @@ Pizza.prototype.calcPrice = function () {
   this.toppings.forEach(function(toppings) {
     this.price += 1;
   })
-  if (this.size === 12) {
-    this.price += 5
-  } else if (this.size === 6) {
+  if (this.size === 24) {
+    this.price += 9;
+  } else if (this.size === 18) {
+    this.price +=7;
+  } else if (this.size === 14) {
+    this.price += 5;
+  } else if (this.size === 11) {
     this.price += 3;
   }
   return this.price;
@@ -22,20 +26,20 @@ $(document).ready(function() {
   $("form#order").submit(function(event) {
     event.preventDefault();
 
-    let toppingsInputs = $("#toppings :checked");
+    let toppingsInputs = $("#toppings:checked").val();
     let toppingsArray = [];
-    let sizeInput = parseInt($("select#size").val());
-    console.log(sizeInput);
+    let sizeInput = $("select#size").val();
 
-    toppingsInputs.forEach(function(toppingsInput) {
-      toppingsArray.push(toppingsInput.val());
-    })
+  for(var i = 0; i < toppingsArray.length; i++) {
+        toppingsArray.push(toppingsInput.val());
+      };
 
-    let ourPizza = new Pizza(toppingsArray, sizeInput)
-    let price = ourPizza.calcPrice();
+      let ourPizza = new Pizza(toppingsArray, sizeInput);
+      let price = ourPizza.calcPrice();
 
-    $("#order").hide();
-    $('#priceOutput').show();
-    $("#output").text();
+      $("#order").hide();
+      $('#priceOutput').show();
+      $("#output").text();
+      console.log(price);
+    });
   });
-});
