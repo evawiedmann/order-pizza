6198d88ba3bacfeecaf488effd3ebfbc53d45ac7
Eva Wiedmann <evawiedmann@gmail.com>
2019-11-08 11:06:38 -0800

    begin to nail down form in html, mess with formatting and category options

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,28 @@
 // Business Logic
-
 function Pizza(toppings,size) {
-this.toppings = toppings;
-this.size = size;
-this.price = 0;
+  this.toppings = toppings;
+  this.size = size;
+  this.price = 0;
 }
 
 Pizza.prototype.calcPrice = function () {
   this.toppings.forEach(function(toppings) {
     this.price += 1;
-  }
+  })
   if (this.size === 12) {
-this.price += 5
-} else if (this.size === 6) {
-  this.price += 3;
-}
-return this.price;
-)
+    this.price += 5
+  } else if (this.size === 6) {
+    this.price += 3;
+  }
+  return this.price;
 };
 
-
 // User Interface
-
 $(document).ready(function() {
-  $("form#").submit(function(event) {
+  $("form#pizza").submit(function(event) {
     event.preventDefault();
 
-    });
+    $("#order").hide();
+    $('#priceOutput').show();
   });
 });
